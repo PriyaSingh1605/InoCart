3c72ee1c93b6a8006f2b48da08f30ca9f7b12b36
Priya Singh <singhshruti4131@gmail.com>
2026-01-17 02:53:52 +0530

    maine message bnaya

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,17 @@
 import { useState } from 'react'
 import HomePage from './pages/HomePage/HomePage'
+import {Route, Routes} from 'react-router-dom'
+import Messages from './pages/Message/Message'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-     <HomePage />
+    <Routes>
+      <Route path="/" element={<HomePage />} /> 
+      <Route path="/messages" element={<Messages />} /> 
+    </Routes>
     </>
   )
 }
